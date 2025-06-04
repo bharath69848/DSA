bee7b894cb4638d7b35188ed3dec23ea7bfa58e9
bharath69848 <bharathpersonal06@gmail.com>
2025-06-04 14:05:28 +0530

    Method-2 Added

--- Recursion/sumofn.py
+++ Recursion/sumofn.py
@@ -1,5 +1,13 @@
+#Method-1
 def sumofn(N,res=0):
     if N==0: return(res)
     res += N
     return sumofn(N-1,res)
-print(sumofn(4))
+print(sumofn(0))
+
+#Method-2
+def sumofn2(n):
+    if n==0:
+        return 0
+    return n+sumofn2(n-1)
+print(sumofn2(4))
