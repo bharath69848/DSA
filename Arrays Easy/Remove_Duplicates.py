2f0ce3e565639ea07a023c5b092dfd2698b4278e
bharath69848 <bharathpersonal06@gmail.com>
2025-06-05 20:48:56 +0530

    Tried this again

--- Arrays Easy/Remove_Duplicates.py
+++ Arrays Easy/Remove_Duplicates.py
@@ -1,7 +1,7 @@
 arr = [1,1,1,1,2,2,2,3,4,5,5,2]
-last_dup = 1
+j = 1
 for i in range(1,len(arr)):
-    if arr[i-1]!=arr[i]:
-        arr[last_dup] = arr[i]
-        last_dup += 1
-print("Array -->",arr)
+    if arr[i-1] != arr[i]:
+        arr[j] = arr[i]
+        j+=1
+print(arr)
