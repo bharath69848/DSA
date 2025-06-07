feb82138dde2ba69314888b117c7cd2faa115408
bharath69848 <bharathpersonal06@gmail.com>
2025-06-07 18:14:36 +0530

    space added

--- PATTERNS/p22.py
+++ PATTERNS/p22.py
@@ -2,5 +2,5 @@ n=int(input("Square Length: "))
 for i in range(2*n-1):
     for j in range(2*n-1):
         top,down,left,right = i,2*n-2-i,j,2*n-2-j
-        print(n-min(top,down,left,right),end="")
+        print(n-min(top,down,left,right),end=" ")
     print()
